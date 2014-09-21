800de72f58c0b60f0822395a985da8c78baddba9
sravi1510 <somu1510@hotmail.com>
2014-09-21 00:01:38 -0700

    changing headers

---
+++ src/com/asu/vcare/SplashActivity.java
@@ -0,0 +1,57 @@
+package com.asu.vcare;
+
+import com.parse.Parse;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SplashActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setTheme(R.style.splashTheme);
+		Parse.initialize(this, "e0O1wPBh8qdsw5KhWhv5Xyc7D5gpv7gq9mGYosLh", "y8hOkkC4SfAegYxKMSN7eaZdHCNrYi1oW3rB2OY8"); 
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		
+		Thread loadingThread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    super.run();
+                    sleep(5000);
+                }catch(Exception e){
+
+                }finally {
+
+                    Intent i = new Intent(SplashActivity.this,
+                    		LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        loadingThread.start();
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.splash, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		
+		return super.onOptionsItemSelected(item);
+	}
+}
