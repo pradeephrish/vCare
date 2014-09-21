2bddedfb0f264eba2bd3c26f3bfc69ec564c62e0
Pradeep Chaudhari <ppchaudh@asu.edu>
2014-09-20 20:12:32 -0700

    DAOManager complete without testing

--- vCare/src/com/asu/dao/DAOManager.java
+++ vCare/src/com/asu/dao/DAOManager.java
@@ -1,12 +1,14 @@
 package com.asu.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.provider.ContactsContract.CommonDataKinds.Event;
-import android.provider.ContactsContract.CommonDataKinds.Organization;
 import android.util.Log;
 
+import com.asu.models.Organization;
 import com.asu.models.User;
+import com.asu.models.Events;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -68,7 +70,7 @@ public class DAOManager {
 	                user.setZipCode(userList.get(0).getString("zipCode"));
 	                user.setEmailID(userList.get(0).getString("emailID"));
 	                user.setBadgeRespect(userList.get(0).getInt("badgeRespect"));
-	                user.setBadgeKindness(userList.get(0).getInt("badgeRespect"));
+	                user.setBadgeKindness(userList.get(0).getInt("badgeKindness"));
 	                user.setBadgeCompassion(userList.get(0).getInt("badgeCompassion"));
 	                user.setBadgeSelfReflection(userList.get(0).getInt("badgeSelfReflection"));
 			 }
@@ -102,20 +104,71 @@ public class DAOManager {
 		return null;
 	}
 	
-	public  List<Event> 	get(String userID){
-		return null;
+	public  List<Events> get(String userID){
+		List<Events> eventStore = new ArrayList<Events>();
+		 ParseQuery<ParseObject> query = ParseQuery.getQuery("EventStore");
+		 query.whereEqualTo("organizerID", userID);
+		 List<ParseObject> eventParseObject;
+		try {
+			eventParseObject = query.find();
+			if(eventParseObject.size() > 0 ){ //should be great than 0
+				 //should be equal to one
+	                Log.d("events", "Retrieved " + eventParseObject.size() + " events");
+	                for (int i = 0; i < eventParseObject.size(); i++) {
+	                	Events  event = new Events();
+	  	                event.setAddress(eventParseObject.get(i).getString("address"));
+	  	                event.setStartDate(eventParseObject.get(i).getDate("startDate"));
+	  	                event.setEndDate(eventParseObject.get(i).getDate("endDate"));
+	  	                event.setEventDescription(eventParseObject.get(i).getString("eventDescription"));
+	  	                event.setEventName(eventParseObject.get(i).getString("eventName"));
+	  	                event.setOrganizationID(eventParseObject.get(i).getString("organizationID"));
+	  	                event.setOrganizerID(userID);
+	  	                eventStore.add(event);
+					}
+			 }
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return eventStore;
 	}
 	
-	public Event createEvent(Event event, String userID){
+	public Events createEvent(Events event, String userID){
+		ParseObject orgStore = new ParseObject("EventStore");
+        orgStore.put("address", event.getAddress()); 
+        orgStore.put("startDate", event.getStartDate());
+        orgStore.put("endDate", event.getEndDate());
+        orgStore.put("eventDescription", event.getEventDescription());
+        orgStore.put("eventName", event.getEventName());
+        orgStore.put("organizationID", event.getOrganizationID());
+        orgStore.put("organizerID", event.getOrganizerID());//mobileNumber
+        orgStore.saveInBackground();
 		return event;
 	}
 	
 	public Organization createOrganization(Organization organization){
+		ParseObject orgStore = new ParseObject("OrganizationStore");
+        orgStore.put("score", organization.getOrganizationName()); 
+        orgStore.put("zipCode", organization.getEmailID());
+        orgStore.put("name", organization.getAddress());
+        orgStore.put("emailID", organization.getContactNumber());
+        orgStore.put("badgeCompassion", organization.getDescription());
+        orgStore.saveInBackground();
 		return organization;
 	}
 	
-	public boolean registerUser(User user){
-		return false;
+	public User registerUser(User user){
+		ParseObject gameScore = new ParseObject("UserStore");
+        gameScore.put("score", user.getMobileNumber());
+        gameScore.put("zipCode", user.getZipCode());
+        gameScore.put("name", user.getName());
+        gameScore.put("emailID", user.getEmailID());
+        gameScore.put("badgeCompassion", user.getBadgeCompassion());
+        gameScore.put("badgeKindness", user.getBadgeKindness());
+        gameScore.put("badgeRespect", user.getBadgeRespect());
+        gameScore.put("badgeSelfReflection", user.getBadgeSelfReflection());
+        gameScore.saveInBackground();
+        return user;
 	}
 	
 	public List<Event> getNearbyDetails(String locationDetails){
